5c00dd026ffacd718e29b613827d96bd112a90f1
Nguyen Duc An <my.skyline.pf@gmail.com>
2022-08-07 20:01:52 +0700

    Redirect to prev page after auth

--- routes/auth.js
+++ routes/auth.js
@@ -9,35 +9,39 @@ const { JWT_REFRESH_TOKEN_SECRET, JWT_ACCESS_TOKEN_SECRET, FRONTEND_URL } =
   process.env;
 const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 365;
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.AUTH_CLIENT_ID,
-      clientSecret: process.env.AUTH_CLIENT_SECRET,
-      callbackURL: process.env.AUTH_REDIRECT_URI,
-      passReqToCallback: true,
-      scope: ['profile', 'email']
-    },
-    async (req, _, __, profile, done) => {
-      console.log(req.headers);
-      const filter = { provider: 'google', providerUserId: profile.id };
-      const data = {
-        provider: 'google',
-        providerUserId: profile.id,
-        firstName: profile.name.familyName,
-        middleName: profile.name.middleName,
-        lastName: profile.name.givenName,
-        email: profile.emails[0].value,
-        profileImage: profile.photos[0].value
-      };
-      const user = await User.findOneAndUpdate(filter, data, {
-        upsert: true,
-        new: true
-      });
-      done(null, user);
-    }
-  )
-);
+function setupGoogleStrategy(req, res, next) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.AUTH_CLIENT_ID,
+        clientSecret: process.env.AUTH_CLIENT_SECRET,
+        callbackURL: process.env.AUTH_REDIRECT_URI,
+        scope: ['profile', 'email']
+      },
+      async (_, __, profile, done) => {
+        const filter = { provider: 'google', providerUserId: profile.id };
+        const data = {
+          provider: 'google',
+          providerUserId: profile.id,
+          firstName: profile.name.familyName,
+          middleName: profile.name.middleName,
+          lastName: profile.name.givenName,
+          email: profile.emails[0].value,
+          profileImage: profile.photos[0].value
+        };
+        const user = await User.findOneAndUpdate(filter, data, {
+          upsert: true,
+          new: true
+        });
+        done(null, {
+          data: user,
+          redirectURL: `${FRONTEND_URL}${req.query.next}`
+        });
+      }
+    )
+  );
+  next();
+}
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -47,15 +51,18 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-router.get('/signin/oauth2/google', passport.authenticate('google'));
+router.get(
+  '/signin/oauth2/google',
+  setupGoogleStrategy,
+  passport.authenticate('google')
+);
 
 router.get(
   '/oauth2/google/redirect',
   passport.authenticate('google'),
   (req, res) => {
-    // console.log(req);
     const refreshToken = jwt.sign(
-      { id: req.user._id },
+      { id: req.user.data._id },
       JWT_REFRESH_TOKEN_SECRET
     );
     res.cookie('refreshToken', refreshToken, {
@@ -64,7 +71,7 @@ router.get(
       sameSite: 'None',
       secure: true
     });
-    res.redirect(FRONTEND_URL);
+    res.redirect(req.user.redirectURL);
   }
 );
 
